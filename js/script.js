936d23cf9946c25dd5b58050484406a5570a88fb
Sobhan <ziaey@gmail.com>
2023-12-24 12:20:16 +0330

    button event is added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const searchInput = document.getElementById("search-input");
 const productItems = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter");
 
 const searchInputEvent = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
-  console.dir(searchValue);
 
   productItems.forEach((product) => {
     const productName = product.children[1].innerText.toLowerCase();
@@ -15,4 +15,25 @@ const searchInputEvent = (event) => {
   });
 };
 
+const filterButton = (event) => {
+  const filter = event.target.dataset.filter;
+
+  
+
+  productItems.forEach((product) => {
+    const category = product.dataset.category;
+    if (filter === "All") {
+      product.style.display = "block";
+    } else {
+      filter === category
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
+
 searchInput.addEventListener("keyup", searchInputEvent);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", filterButton);
+});
