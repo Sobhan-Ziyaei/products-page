55aeae701049e25c2a1a0f0ce95529950ecd4179
Sobhan <ziaey@gmail.com>
2023-12-24 13:47:56 +0330

    complete app

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 const searchInput = document.getElementById("search-input");
 const productItems = document.querySelectorAll(".product-item");
 const buttons = document.querySelectorAll(".filter");
+const price = document.getElementById("search-price").querySelector("button");
 
-const changeClass = (filter)=>{
-    buttons.forEach((button)=>{
-        if (button.dataset.filter === filter) {
-            button.classList.add("selected")
-        }else {
-            button.classList.remove("selected")
-        }
-    })
-}
+const changeClass = (filter) => {
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filter) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
+};
 
 const searchInputEvent = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
@@ -27,7 +28,7 @@ const searchInputEvent = (event) => {
 
 const filterButton = (event) => {
   const filter = event.target.dataset.filter;
-  changeClass(filter)
+  changeClass(filter);
   productItems.forEach((product) => {
     const category = product.dataset.category;
     if (filter === "All") {
@@ -40,7 +41,23 @@ const filterButton = (event) => {
   });
 };
 
+const searchPrice = (event) => {
+  const price = +event.target.parentElement.children[0].value;
+  productItems.forEach((product) => {
+    const productPrice = product.children[2].innerText;
+    const productPriceFilter = +productPrice.slice(1, productPrice.length);
+    if (!price) {
+      product.style.display = "block";
+    } else {
+      price === productPriceFilter
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
+
 searchInput.addEventListener("keyup", searchInputEvent);
+price.addEventListener("click", searchPrice);
 
 buttons.forEach((button) => {
   button.addEventListener("click", filterButton);
