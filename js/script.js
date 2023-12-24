65f5679a8711b1d8852a12b613119edd38bd7464
Sobhan <ziaey@gmail.com>
2023-12-24 11:34:00 +0330

    search product event is added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const searchInput = document.getElementById("search-input");
+const productItems = document.querySelectorAll(".product-item");
+
+const searchInputEvent = (event) => {
+  const searchValue = event.target.value.toLowerCase().trim();
+  console.dir(searchValue);
+
+  productItems.forEach((product) => {
+    const productName = product.children[1].innerText.toLowerCase();
+    if (productName.includes(searchValue)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+};
+
+searchInput.addEventListener("keyup", searchInputEvent);
